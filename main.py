2d7e20018f146add5f7dfcf01979a43a5322285a
Felipe-UNALM <felipefarro.ao.1@gmail.com>
2026-06-28 01:19:06 -0500

    Update main.py, actualizando

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-import os
 import subprocess
 import database
+import convertidor  # <- Añade esta importación
 
 def ejecutar_pipeline():
-    print("--- 🚀 Iniciando Proyecto: Monitor de Desinformación ---")
-    
-    # 1. Ejecutar scrapers
-    print("--- 1. Extrayendo datos...")
+    print("🚀 Iniciando extracción...")
     subprocess.run(["python", "scraper_fuente1.py"])
     subprocess.run(["python", "scraper_fuente2.py"])
     
-    # 2. Limpieza (si `limpieza.py` requiere ejecución directa)
-    print("--- 2. Limpiando datos...")
-    subprocess.run(["python", "limpieza.py"])
+    print("🚀 Convirtiendo datos a formato compatible...")
+    convertidor.convertir_json_a_csv() # <- Nueva línea
     
-    # 3. Cargar a Base de Datos
-    print("--- 3. Creando Base de Datos...")
+    print("🚀 Cargando a Base de Datos...")
     database.crear_base_de_datos()
     database.integrar_csv_a_sqlite()
     
-    print("--- ✅ Pipeline completado exitosamente. ---")
-    print("--- Lanzando Dashboard... ---")
-    
-    # 4. Lanzar Streamlit
+    print("✅ Pipeline completado. Lanzando Dashboard...")
     subprocess.run(["python", "-m", "streamlit", "run", "app.py"])
 
 if __name__ == "__main__":
