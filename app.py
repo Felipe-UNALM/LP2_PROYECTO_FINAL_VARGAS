5faad04c5908f4a1008f8ae24af1be4e562731f0
IsaacAlvarezCaja2026 <20221389@lamolina.edu.pe>
2026-06-28 15:31:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,29 @@ import pandas as pd
 import sqlite3
 import plotly.express as px
 
+# 1. Conexión y extracción de datos desde el motor relacional (SQLite)
 conn = sqlite3.connect("monitor_desinformacion.db")
 df = pd.read_sql_query("SELECT * FROM articulos", conn)
 conn.close()
 
+# 2. Configuración de la cabecera principal de la interfaz web
 st.title("📊 Monitor de Desinformación Científica")
 
-# Métricas
+# 3. Construcción de Métricas (KPIs)
 col1, col2, col3 = st.columns(3)
-col1.metric("Total Artículos", len(df))
-# Aquí buscamos "ALTO RIESGO" en mayúsculas como pediste
-alertas = len(df[df["clasificacion"] == "ALTO RIESGO"])
-col2.metric("Alertas de Alto Riesgo 🚨", alertas)
-col3.metric("Promedio IRD", round(df["puntaje_ird"].mean(), 2))
 
-# Gráfico
-fig = px.bar(df, x="titulo", y="puntaje_ird", color="clasificacion")
+col1.metric("Total Artículos", len(df)) # KPI 1
+
+# Conteo de alertas críticas (Filtro por clasificación condicional)
+alertas = len(df[df["clasificacion"] == "ALTO RIESGO"])
+col2.metric("Alertas de Alto Riesgo 🚨", alertas) #KPI 2
+col3.metric("Promedio IRD", round(df["puntaje_ird"].mean(), 2)) # KPI 3
+
+# 4. Gráfico de barras para el Dashboard
+fig = px.bar(
+    df, 
+    x="titulo", 
+    y="puntaje_ird", 
+    color="clasificacion"
+)
 st.plotly_chart(fig)
